890a5c9863a514f9f4488cc0e2cb8f4670a1be8d
Furkan Soylu <furkansylu97@gmail.com>
2025-06-20 20:02:28 +0300

    React Router page management setup was performed and Project infrastruce was created

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import Home from "./pages/HomePage/Home";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Furkan Soylu Portfolio</h1>
+      <Routes>
+        <Route path="/" element={<Home /> } />
+      </Routes>
     </>
   );
 }
