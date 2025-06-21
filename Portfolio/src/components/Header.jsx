3b2562909dff8390d10d41355fbf6a71dffdfd6e
Furkan Soylu <furkansylu97@gmail.com>
2025-06-21 19:27:16 +0300

    Header component contents were written

--- Portfolio/src/components/Header.jsx
+++ Portfolio/src/components/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import FurkanSoylu from '../images/FurkanSoylu.svg'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='flex justify-center items-center p-5 bg-primary h-36'>
+      <div>
+        <Link to='/'>
+          <img className='h-36 w-full' src={ FurkanSoylu } alt="Furkan Soylu" />
+        </Link>
+      </div>
+    </div>
   )
 }
 
--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': "#05445e",
+        'secondary': "#02fced",
+      },
+    },
   },
   plugins: [],
 };
