15e6f1cf623fabe8734749798b24dcd6f8648261
Furkan Soylu <furkansylu97@gmail.com>
2025-06-23 18:18:38 +0300

    LottieFiles animations integration to project was performed

--- Portfolio/src/pages/HomePage/About.jsx
+++ Portfolio/src/pages/HomePage/About.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import SectionTitle from '../../components/SectionTitle'
+import React from "react";
+import SectionTitle from "../../components/SectionTitle";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 function About() {
   return (
     <div>
-        <SectionTitle title="About" />
+      <SectionTitle title="About Me" />
+      <div className="flex h-[50vh]">
+        <DotLottieReact
+          src="https://lottie.host/50888e59-bf72-47b5-8a35-8f6f9b3e691c/5Vk5cWtcEP.lottie"
+          loop
+          autoplay
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
