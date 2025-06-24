b04c6250a5bd61fe25e06d29bbcbc038b59bb478
Furkan Soylu <furkansylu97@gmail.com>
2025-06-24 13:00:42 +0300

    About me section contents was completed

---
+++ Portfolio/src/components/Icons.jsx
@@ -0,0 +1,37 @@
+import Bootstrap from "../images/icons/bootstrap.png";
+import Css from "../images/icons/css-3.png";
+import Django from "../images/icons/django.png";
+import Figma from "../images/icons/figma.png";
+import Git from "../images/icons/git.png";
+import Github from "../images/icons/github.png";
+import Html from "../images/icons/html-5.png";
+import Javascript from "../images/icons/javascript.png";
+import MaterialUi from "../images/icons/material-ui.png";
+import MongoDB from "../images/icons/mongodb.png";
+import Python from "../images/icons/python.png";
+import Reactlogo from "../images/icons/react.png";
+import Scss from "../images/icons/scss.png";
+import Tailwind from "../images/icons/tailwind.png";
+import Typescript from "../images/icons/typescript.png";
+import AdobePhotoshop from "../images/icons/adobe-photoshop.png";
+
+const Icons = [
+  { src: Javascript, alt: "Javascript", name: "JAVASCRIPT" },
+  { src: Typescript, alt: "Typescript", name: "TYPESCRIPT" },
+  { src: Python, alt: "Python", name: "PYTHON" },
+  { src: Html, alt: "Html", name: "HTML-5" },
+  { src: Css, alt: "Css", name: "CSS" },
+  { src: Reactlogo, alt: "React", name: "REACT" },
+  { src: Django, alt: "Django", name: "DJANGO" },
+  { src: MongoDB, alt: "MongoDB", name: "MongoDB" },
+  { src: Git, alt: "Git", name: "GIT" },
+  { src: Github, alt: "Github", name: "GITHUB" },
+  { src: MaterialUi, alt: "MaterialUi", name: "MaterialUi" },
+  { src: Tailwind, alt: "Tailwind", name: "TAILWIND" },
+  { src: Bootstrap, alt: "Bootstrap", name: "BOOTSTRAP" },
+  { src: Scss, alt: "Scss", name: "SCSS" },
+  { src: Figma, alt: "Figma", name: "FIGMA" },
+  { src: AdobePhotoshop, alt: "AdobePhotoshop", name: "PHOTOSHOP" },
+];
+
+export default Icons;
--- Portfolio/src/pages/HomePage/About.jsx
+++ Portfolio/src/pages/HomePage/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SectionTitle from "../../components/SectionTitle";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import icons from "../../components/Icons";
 
 function About() {
   return (
@@ -8,7 +9,7 @@ function About() {
       <div>
         <SectionTitle title="About Me" />
         <div className="flex w-full">
-          <div className="h-[50vh] w-1/2 pr-52">
+          <div className="h-[45vh] w-1/2 pr-52">
             <DotLottieReact
               src="https://lottie.host/50888e59-bf72-47b5-8a35-8f6f9b3e691c/5Vk5cWtcEP.lottie"
               loop
@@ -34,6 +35,26 @@ function About() {
               individual committed to learning and growing in the technology
               field.
             </p>
+          </div>
+        </div>
+        <div className="my-3 py-3">
+          <h1 className="text-white text-xl my-3 ">
+            I’ve been focusing on the following technologies in my development
+            process:
+          </h1>
+          <div className="flex flex-wrap w-4/5 mx-auto justify-center gap-10 mt-3">
+            {icons.map((icon, index) => (
+              <div className="flex flex-col items-center justify-center text-center" key={index}>
+                <img
+                  src={icon.src}
+                  alt={icon.alt}
+                  className="w-16 h-16 object-contain"
+                />
+                <div className="text-center">
+                  <h1 className="font-medium mt-1">{icon.name}</h1>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
