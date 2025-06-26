76f65f1bd3c4ba5b5c9cafd8f1226d3c895a7e53
Furkan Soylu <furkansylu97@gmail.com>
2025-06-26 15:50:18 +0300

    A better responsive design was achieved with the Swiper library

--- Portfolio/src/pages/HomePage/KnowledgeAndExperiences.jsx
+++ Portfolio/src/pages/HomePage/KnowledgeAndExperiences.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import { FreeMode } from "swiper/modules";
 import SectionTitle from "../../components/SectionTitle";
 import { experiences } from "../../resources/experiences";
 
@@ -9,29 +13,59 @@ function KnowledgeAndExperiences() {
     <section className="h-svh lg:h-lvh">
       <div>
         <SectionTitle title="Experiences & Education" />
-        <div className="flex py-16 lg:py-32 gap-5 lg:gap-10 ">
-          <div className="flex flex-col gap-3 lg:gap-6 border-l-2 lg:border-l-4 border-cyan-800 w-1/3 lg:w-1/6 h-1/3 lg:h-1/6">
-            {experiences.map((experience, index) => (
-              <div
-                key={experience.id}
-                className="cursor-pointer"
-                onClick={() => {
-                  setSelectedItemIndex(index);
-                }}
+        <div className="flex flex-col lg:flex-row py-8 lg:py-32 gap-5 lg:gap-10">
+          <div className="lg:w-1/6">
+            <div className="w-100 lg:hidden">
+              <Swiper
+                slidesPerView={1.4}
+                spaceBetween={10}
+                freeMode={true}
+                centeredSlides={true}
+                modules={[FreeMode]}
+                centeredSlidesBounds={true}
+                className="w-full"
               >
-                <h1
-                  className={`text-[8px] lg:text-xl px-2 lg:px-5 ${
-                    selectedItemIndex === index
-                      ? "text-secondary border-secondary border-l-2 lg:border-l-4 -ml-[4px] bg-[#155e75] py-2 lg:py-3"
-                      : "text-white"
-                  }`}
+                {experiences.map((experience, index) => (
+                  <SwiperSlide key={experience.id}>
+                    <div
+                      className={`cursor-pointer px-2 py-2 rounded-md transition-all duration-300 ${
+                        selectedItemIndex === index
+                          ? "bg-tertiary text-white font-bold"
+                          : "text-white bg-primary"
+                      }`}
+                      onClick={() => setSelectedItemIndex(index)}
+                    >
+                      <span className="text-[10px]">{experience.period}</span>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+
+            <div className="hidden lg:flex flex-col gap-6 border-l-4 border-cyan-800 h-1/6">
+              {experiences.map((experience, index) => (
+                <div
+                  key={experience.id}
+                  className="cursor-pointer"
+                  onClick={() => {
+                    setSelectedItemIndex(index);
+                  }}
                 >
-                  {experience.period}
-                </h1>
-              </div>
-            ))}
+                  <h1
+                    className={`text-xl px-5 ${
+                      selectedItemIndex === index
+                        ? "text-secondary border-secondary border-l-4 -ml-[4px] bg-[#155e75] py-3"
+                        : "text-white"
+                    }`}
+                  >
+                    {experience.period}
+                  </h1>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="flex flex-col w-4/6 gap-1">
+
+          <div className="flex flex-col w-full lg:w-4/6 gap-1">
             <h1 className="flex text-quaternary text-[13px] lg:text-xl font-medium">
               {experiences[selectedItemIndex].company}
               <span className="text-slate-700 text-[13px] lg:text-xl font-normal">
